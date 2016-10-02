f42c2890112da5308941e84fb307501db5d14280
gaoboallen <bg447@cornell.edu>
2016-10-02 14:39:06 -0400

    pytree

--- pytree.py
+++ pytree.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
-import subprocess
 import sys
+import os
+import re
 
+# Recursive function
+def dfsTree(curPath, prefix):
+    files = [x for x in os.listdir(curPath) if x[0] != '.']
+    dirN, fileN = 0, 0
+    for i, fname in enumerate(files):
+        if i < len(files) - 1:
+            print(prefix + "├── " + str(fname))
+            subdirPrefix = "│   "
+        else:
+            print(prefix + "└── " + str(fname))
+            subdirPrefix = "    "
+        if os.path.isfile(os.path.join(curPath, fname)):
+            fileN += 1
+        else:
+            dirN += 1
+            tdirN, tfileN = dfsTree(os.path.join(curPath, fname), prefix + subdirPrefix)
+            dirN, fileN = dirN + tdirN, fileN + tfileN
+    return dirN, fileN
 
-# YOUR CODE GOES here
+def tree(path):
+	print(path)
+	dirN, fileN = dfsTree(path, "")
+	print("\n")
+	print(str(dirN) + " directories, " + str(fileN) + " files\n")
 
 
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    path = sys.argv[1] if len(sys.argv) > 1 else "."
+    tree(path)
